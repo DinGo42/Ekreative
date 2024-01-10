3437847f5a66e4f674eb876b4fb9a2fbf7f2d66b
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-01-10 16:02:02 +0200

    done without routing

--- finsweet-pure-ts/src/components/features-card.ts
+++ finsweet-pure-ts/src/components/features-card.ts
@@ -2,6 +2,7 @@ type FeaturesCardProps = {
   iconSrc: string;
   title: string;
   text: string;
+  cardColor?: string;
 };
 
 class FeaturesCard extends HTMLElement {
@@ -11,7 +12,7 @@ class FeaturesCard extends HTMLElement {
     shadowRoot.innerHTML = `
       <link rel="stylesheet" href="dist/output.css" />
 
-      <div class='flex flex-col pl-12 pr-8 pb-12 pt-[52px] gap-3 bg-white-1000'>
+      <div id='card' class='flex flex-col pl-12 pr-8 pb-12 pt-[52px] gap-3 bg-white-1000'>
         <img id='card-icon-src' class='size-8 mb-2'/>
         <span id='card-title' class='text-xs text-blue-1000'></span>
         <span id='card-text' class='text-bs-1 text-blue-1000 text-opacity-70'></span>
@@ -31,21 +32,30 @@ class FeaturesCard extends HTMLElement {
     iconSrc,
     text,
     title,
+    cardColor,
   }: FeaturesCardProps) {
     const shadowRoot = this.shadowRoot;
 
     if (shadowRoot) {
+      const card = shadowRoot.querySelector('#card');
+
       const icon: HTMLImageElement | null =
         shadowRoot.querySelector('#card-icon-src');
       icon!.src = iconSrc || '';
 
       shadowRoot.querySelector('#card-title')!.textContent = title || '';
       shadowRoot.querySelector('#card-text')!.textContent = text || '';
+
+      cardColor?.split(' ').forEach((style) => {
+        console.log(style, card);
+        card!.classList.remove('bg-white-1000');
+        card!.classList.add(style);
+      });
     }
   }
 
   static get observedAttributes() {
-    return ['title', 'iconSrc', 'text'];
+    return ['title', 'iconSrc', 'text', 'cardColor'];
   }
 
   attributeChangedCallback(oldValue: string, newValue: string) {
@@ -54,6 +64,7 @@ class FeaturesCard extends HTMLElement {
         iconSrc: this.getAttribute('iconSrc') || '',
         title: this.getAttribute('title') || '',
         text: this.getAttribute('text') || '',
+        cardColor: this.getAttribute('cardColor') || '',
       });
     }
   }

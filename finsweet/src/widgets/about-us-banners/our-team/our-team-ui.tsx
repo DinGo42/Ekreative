e40e284189ca96e5f0e741dbedd7b54afb036404
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-01-16 21:47:09 +0200

    removed useless fragment

--- finsweet/src/widgets/about-us-banners/our-team/our-team-ui.tsx
+++ finsweet/src/widgets/about-us-banners/our-team/our-team-ui.tsx
@@ -25,26 +25,24 @@ export const OurTeamUI: FC = () => (
           titleClassName="text-h6"
           className="items-center text-center pl-11 pr-11 pt-12 pb-8 bg-white-1000 whitespace-nowrap"
           icon={
-            <>
-              <Image
-                src={avatarSRC}
-                alt="team"
-                className="rounded-full overflow-hidden transition-all duration-700 w-[10rem] h-[10rem] group"
-                style={{ objectFit: 'cover' }}
+            <Image
+              src={avatarSRC}
+              alt="team"
+              className="rounded-full overflow-hidden transition-all duration-700 w-[10rem] h-[10rem] group"
+              style={{ objectFit: 'cover' }}
+            >
+              <div
+                className={twMerge(
+                  'overflow-hidden duration-700 transition-all flex justify-center gap-3 items-end pb-10 group-hover:translate-y-0 w-40 h-40 translate-y-full bg-[linear-gradient(348.37deg,_#5239FA_10.97%,_#ffffff00_60.2%)]'
+                )}
               >
-                <div
-                  className={twMerge(
-                    'overflow-hidden duration-700 transition-all flex justify-center gap-3 items-end pb-10 group-hover:translate-y-0 w-40 h-40 translate-y-full bg-[linear-gradient(348.37deg,_#5239FA_10.97%,_#ffffff00_60.2%)]'
-                  )}
-                >
-                  <SocialMedia
-                    facebook={facebookLink}
-                    twitter={twitterLink}
-                    linkedIn={linkedIn}
-                  />
-                </div>
-              </Image>
-            </>
+                <SocialMedia
+                  facebook={facebookLink}
+                  twitter={twitterLink}
+                  linkedIn={linkedIn}
+                />
+              </div>
+            </Image>
           }
           key={index}
         />

905fa75a696a4d240a00f18f00fdeb93fc12b169
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-01-25 16:09:00 +0200

    changed a tag to link tag

--- apps/finsweet/src/shared/components/footer/contacts/contacts.tsx
+++ apps/finsweet/src/shared/components/footer/contacts/contacts.tsx
@@ -1,18 +1,19 @@
 import { FC } from "react";
+import { Link } from "../../links";
 
 export const Contacts: FC = () => (
   <div className="bg-yellow-90 text-label-main max-tabletS:w-full max-phoneSPlus:hidden phoneM:pl-6 tabletS:ml-20 tabletS:pr-28 desktopS:ml-40 flex w-fit gap-7 pb-2 pl-3 pt-4">
     <div className="flex flex-col">
       <span>Email me at</span>
-      <a className="text-body-main" href="mailto:contact@website.com">
+      <Link className="text-body-main" href="mailto:contact@website.com">
         contact@website.com
-      </a>
+      </Link>
     </div>
     <div className="flex flex-col">
       <span>Call us</span>
-      <a className="text-body-main" href="tel:+0927627728525">
+      <Link className="text-body-main" href="tel:+0927627728525">
         0927-6277-28525
-      </a>
+      </Link>
     </div>
   </div>
 );

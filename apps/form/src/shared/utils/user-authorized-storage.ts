8b11cf2e10f102a88ff53d7206ee9ea8572461d9
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-02-21 13:07:49 +0200

    fixed issue

--- apps/form/src/shared/utils/user-authorized-storage.ts
+++ apps/form/src/shared/utils/user-authorized-storage.ts
@@ -1,3 +1,4 @@
+"use client";
 import { localStorageUtilsGenerator } from "./local-storage-utils-generator";
 
 export const userAuthorizedStore = localStorageUtilsGenerator<boolean>("is_authorized");
--- apps/form/src/shared/utils/user-storage.ts
+++ apps/form/src/shared/utils/user-storage.ts
@@ -1,3 +1,4 @@
+"use client";
 import { localStorageUtilsGenerator } from "./local-storage-utils-generator";
 
 export const userInfoStorage = localStorageUtilsGenerator<{
